b0c6f77a874c709a02c3fa9a4fe38bfdddc0f27e
ivan <ilee.4123@gmail.com>
2018-12-03 00:17:57 -0500

    - Maximum number of threads to run the calculation is 100. Made it so that, if the number of random walks is large, the work is divided between the 100 threads. Still needs work, there probably is a better way of doing this. Also, if the number is odd, the amount of work could be more than needed. - Created a bufferwriter function that creates a file and stores the results. Only holds the numbers for now. Stores a lot more than the Arraylist I had earlier but is a lot slower. 1 million results can be stored in the file, while the Arraylist crashes when a max is reached. Arraylist is great for testing purposes.

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,42 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Shared.input();
         createThreads();
+        double startTime = System.currentTimeMillis();
 
         for (int i = 0; i < ParallelThreads.size(); i++) { //starts all Threads
             ParallelThreads.get(i).start();
         }
 
+        for (int i = 0; i < Shared.numThreads; ++i) {
+            Thread thread = ParallelThreads.get(i);
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
         Shared.mutex.acquire(); //main waits for threads to finish.
-        System.out.println("Running time: " + (System.currentTimeMillis()/1000) + " seconds");
+        double endTime = System.currentTimeMillis();
+        System.out.println("Results: " + Shared.results);
+        System.out.println("Running time: " + (endTime - startTime)/1000 + " seconds");
 
     }
 
-    static ArrayList<Thread> ParallelThreads = new ArrayList<Thread>(); // creates Threads.
+    static ArrayList<Thread> ParallelThreads = new ArrayList<>(); // creates Threads.
     public static void createThreads() {
-        int maxWalks = Shared.M;
+        double maxWalks = Shared.numThreads;
         for (int i = 0; i < maxWalks; i++){
             ParallelThreads.add(new Thread(new ParallelWalks(i+1)));
         }
@@ -29,7 +47,13 @@ public class Main {
 
 class Shared{ //class of shared variables
     static int k, u, d, M;
+    static double numThreads, walks;
+    static ArrayList<Integer> results = new ArrayList<>(); //list of results
+    //static int[] n = new int[M];
     static Semaphore mutex = new Semaphore(0); //main waits for threads to finish.
+    //static File file = new File("test.txt");
+
+
 
     static public void input() {
         Scanner reader = new Scanner(System.in);  // Reading from System.in
@@ -60,7 +84,40 @@ class Shared{ //class of shared variables
             System.out.println("M must be greater 0, try again");
             M = reader.nextInt();
         }
+
+        if(M < 100){ //if the number of walks is less than 100, run in one thread
+            numThreads = 1;
+            walks = M;
+        }
+        else {
+            double remainder = Math.ceil(M % 100);
+            numThreads = M / 100 + remainder; //if M is greater than 100, divide work into number of threads
+            if(numThreads > 100){
+                System.out.println("Max threads reached. " );
+                numThreads = 100;
+                walks = Math.ceil(M/100);
+                System.out.println("Num of threads: " + numThreads);
+            }
+            else{
+                walks = Math.ceil(M/numThreads);
+                System.out.println("Num of threads: " + numThreads);
+            }
+        }
         reader.close();
+    }
+
+    static public void writeFile(int n){
+        String r = Integer.toString(n);
+        try(FileWriter fw = new FileWriter("test.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println(r);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
 
